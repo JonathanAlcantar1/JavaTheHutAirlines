d5fac9ffde436eefcf0dab93fa4ccde019f8063c
ricky <rickyramos834@gmail.com>
2023-11-04 20:05:37 -0700

    Manager --> Created method to check username & password ManagerController --> Made the login text to check the input to the validator from the Manager class, which returns a boolean

---
+++ src/main/java/com/example/javathehutair/Manager.java
@@ -0,0 +1,34 @@
+package com.example.javathehutair;
+
+import java.sql.*;
+
+public class Manager {
+    private final String url = "jdbc:mysql://airlinedatabase.ceof6ckatc9m.us-east-2.rds.amazonaws.com:3306/airlineDatabase";
+    private final String username = "admin";
+    private final String password = "!Javathehut23";
+    private Connection connection;
+    private String query;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+        public boolean loginChecker(String username, String password) throws SQLException {
+            String dbUsername, dbPassword;
+            connection = DriverManager.getConnection(url, username, password);
+
+            query = "SELECT * FROM airlineDatabase.adminTable WHERE username LIKE ? & password LIKE ?";
+
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                dbUsername = resultSet.getString("username");
+                dbPassword = resultSet.getString("password");
+                if(!dbUsername.equals(username) && !dbPassword.equals(password)){
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+            return true;
+        }
+}
--- src/main/java/com/example/javathehutair/ManagerLoginController.java
+++ src/main/java/com/example/javathehutair/ManagerLoginController.java
@@ -1,8 +1,12 @@
 package com.example.javathehutair;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
 
 public class ManagerLoginController {
 
@@ -12,6 +16,27 @@ public class ManagerLoginController {
     private TextField passwordTxt;
     @FXML
     private Button loginButton;
+    private String username, password;
+    private Manager managerHelper;
+
+    void clickLogin(ActionEvent event) throws SQLException {
+        username = usernameTxt.getText();
+        password = passwordTxt.getText();
+
+        if(managerHelper.loginChecker(username, password)){
+            // open next manager scene to display what the manager can see/do
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(("tes"));
+            alert.setContentText("Username and password were found");
+            alert.showAndWait();
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(("Invalid Login Credentials"));
+            alert.setContentText("Username or Password is incorrect");
+            alert.showAndWait();
+        }
+    }
 
 
 }
