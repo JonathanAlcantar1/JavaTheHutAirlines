5e959ae5e4e23a87146ea5c251b10fed0a2371ff
ricky <rickyramos834@gmail.com>
2023-11-07 15:51:33 -0800

    Unversioned Files

--- src/main/java/com/example/javathehutair/Manager.java
+++ src/main/java/com/example/javathehutair/Manager.java
@@ -4,8 +4,8 @@ import java.sql.*;
 
 public class Manager {
     private final String url = "jdbc:mysql://airlinedatabase.ceof6ckatc9m.us-east-2.rds.amazonaws.com:3306/airlineDatabase";
-    private final String username = "admin";
-    private final String password = "!Javathehut23";
+    private final String user = "admin";
+    private final String pass = "!Javathehut23";
     private Connection connection;
     private String query;
     private PreparedStatement preparedStatement;
@@ -13,11 +13,13 @@ public class Manager {
 
     public boolean loginChecker(String username, String password) throws SQLException {
         String dbUsername, dbPassword;
-        connection = DriverManager.getConnection(url, username, password);
+        connection = DriverManager.getConnection(url, user, pass);
 
-        query = "SELECT * FROM airlineDatabase.managerCredentialTable WHERE username LIKE ? & password LIKE ?";
+        query = "SELECT * FROM airlineDatabase.managerCredentialTable WHERE (username LIKE ?) AND (password LIKE ?)";
 
         preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
         resultSet = preparedStatement.executeQuery();
         if(resultSet.next()){
             dbUsername = resultSet.getString("username");
